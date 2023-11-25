1d0cc47a192b5782bc22eec55acb5bcf5f0e99e6
ziko199 <zakaria.jouahri@outlook.de>
2023-11-25 11:31:06 +0000

    refactor and write documentation to the code HTMLWord.py

--- data_extractor/code/rule_based_pipeline/rule_based_pipeline/HTMLWord.py
+++ data_extractor/code/rule_based_pipeline/rule_based_pipeline/HTMLWord.py
@@ -4,21 +4,27 @@
 # Author : Ismail Demir (G124272)
 # Date   : 12.06.2020
 #
-# Note   : 1 HTMLItem consistens of * HTMLWords
+# Note   : 1 HTMLItem consists of * HTMLWords
 # ============================================================================================================================
 
-from globals import *
+
 from Rect import *
 
 
 class HTMLWord:
-	txt		= None
-	rect	= None
-	item_id	= None # to which HTMLItem id does this word belong?
-	
-	
-	def __init__(self):
-		self.txt = ''
-		self.rect = Rect(99999,99999,-1,-1)
-		self.item_id = -1
+    """
+    Represents a word in an HTML context, typically extracted from a PDF file.
 
+    Attributes:
+        txt (str): The text content of the word.
+        rect (Rect): The rectangle coordinates of the word, an instance of the Rect class.
+        item_id (int): The ID of the HTMLItem to which this word belongs.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the HTMLWord class.
+        """
+        self.txt = ''
+        self.rect = Rect(99999, 99999, -1, -1)
+        self.item_id = -1
