c70106ca9c85af80b5c1bfb7d3ddac53ab4ce1c6
ziko199 <zakaria.jouahri@outlook.de>
2023-11-25 14:38:12 +0000

    refactor and write documentation to the code AnalyzerDirectory.py

--- data_extractor/code/rule_based_pipeline/rule_based_pipeline/AnalyzerDirectory.py
+++ data_extractor/code/rule_based_pipeline/rule_based_pipeline/AnalyzerDirectory.py
@@ -7,91 +7,113 @@
 # Note   : 1 AnalyzerDirectory refers to * AnalyzerPage (one for each HTMLPage in that directory, resp. pdf-file)
 # ============================================================================================================================
 
-
-from AnalyzerPage import *
-from HTMLDirectory import *
+from AnalyzerPage import AnalyzerPage
+from config import global_analyze_multiple_pages_at_one, global_ignore_all_years
+from globals import print_verbose
+from HTMLDirectory import HTMLDirectory
+from HTMLPage import HTMLPage
+from KPIMeasure import KPIMeasure
 
 
 class AnalyzerDirectory:
-	htmldirectory	= None
-	analyzer_page	= None
-	default_year 	= None
+    """
+    Represents an analyzer directory that contains methods for finding KPIs within the directory.
 
+    Attributes:
+        html_directory (HTMLDirectory): The HTML directory associated with the analyzer directory.
+        analyzer_page (list): List of AnalyzerPage objects for each HTML page in the directory.
+        default_year: The default year value.
+    Methods:
+        fix_src_name(kpi_measures): Fixes the source file name for a list of KPI measures.
+        find_kpis(kpi_specs): Finds KPIs within the entire analyzer directory.
+        find_multiple_kpis(kpi_specs_list): Finds multiple KPIs within the entire analyzer directory.
+    """
+    html_directory = None
+    analyzer_page = None
+    default_year = None
 
-	def __init__(self, htmldirectory, default_year):
-		self.htmldirectory	= htmldirectory
-		self.analyzer_page = []
-		for i in range(len(self.htmldirectory.htmlpages)):
-			p = htmldirectory.htmlpages[i]
-			self.analyzer_page.append(AnalyzerPage(p, default_year))
-			if(config.global_analyze_multiple_pages_at_one and i < len(self.htmldirectory.htmlpages) - 1):
-				p_mult = HTMLPage.merge(p, htmldirectory.htmlpages[i+1])
-				self.analyzer_page.append(AnalyzerPage(p_mult, default_year))
-		self.default_year = default_year
-		
+    def __init__(self, html_directory, default_year):
+        """
+        Initializes an AnalyzerDirectory.
 
-	"""
-	TODO : Probably implementation not neccessary
-	def adjust_scores_by_value_preference(self, lst, kpispecs):
-		if(kpispecs.value_preference == 1.0 or len(lst) < 2):
-			return lst
-			
-		min_val = 
-		for k in lst:
-			...
-	"""
-			
-		
-		
-	def fix_src_name(self, kpi_measures):
-		print_verbose(3, "self.htmldirectory.src_pdf_filename="+self.htmldirectory.src_pdf_filename)
-		res = []
-		for k in kpi_measures:
-			k.set_file_path(self.htmldirectory.src_pdf_filename)
-			res.append(k)
-		return res
-		
-	
-	
-	def find_kpis(self, kpispecs):
-		# find all possible occurenes of kpi on all pages
-		
-		
-		res = []
-		
-		for a in self.analyzer_page:
-			res.extend(a.find_kpis(kpispecs))
-			
-		if(config.global_ignore_all_years):
-			res = KPIMeasure.remove_all_years(res)
+        Args:
+            html_directory (HTMLDirectory): The HTML directory.
+            default_year: The default year value.
+        """
+        self.html_directory = html_directory
+        self.analyzer_page = []
+        for i in range(len(self.html_directory.htmlpages)):
+            p = html_directory.htmlpages[i]
+            self.analyzer_page.append(AnalyzerPage(p, default_year))
+            if global_analyze_multiple_pages_at_one and i < len(self.html_directory.htmlpages) - 1:
+                p_mult = HTMLPage.merge(p, html_directory.htmlpages[i + 1])
+                self.analyzer_page.append(AnalyzerPage(p_mult, default_year))
+        self.default_year = default_year
 
-		#print("\n\n\n1:"+str(res))
-		
-		res = KPIMeasure.remove_duplicates(res)
+    """
+    TODO : Probably implementation not necessary
+    def adjust_scores_by_value_preference(self, lst, kpi_specs):
+        if(kpi_specs.value_preference == 1.0 or len(lst) < 2):
+            return lst
 
-		#print("\n\n\n2:"+str(res))
+        min_val = 
+        for k in lst:
+            ...
+    """
 
-		res = KPIMeasure.remove_bad_scores(res, kpispecs.minimum_score)		
-		
+    def fix_src_name(self, kpi_measures):
+        """
+        Fixes the source file name for a list of KPI measures.
 
+        Args:
+            kpi_measures (list): List of KPI measures.
+        Returns:
+            list: List of KPI measures with fixed source file names.
+        """
+        print_verbose(3, "self.html_directory.src_pdf_filename=" + self.html_directory.src_pdf_filename)
+        res = []
+        for k in kpi_measures:
+            k.set_file_path(self.html_directory.src_pdf_filename)
+            res.append(k)
+        return res
 
-		return res
-	
-	
-	def find_multiple_kpis(self, kpispecs_lst):
-		res = []
-		
-		for k in kpispecs_lst:
-			res.extend(self.find_kpis(k))
-			
+    def find_kpis(self, kpi_specs):
+        # find all possible occurrences of kpi on all pages
+        """
+        Finds KPIs within the entire analyzer directory.
 
-		
-		res = KPIMeasure.remove_bad_years(res, self.default_year)
-		
-		res = KPIMeasure.remove_duplicates(res)
-		
-		res = self.fix_src_name(res)
-		
-		
-		
-		return res
+        Args:
+            kpi_specs (KPISpecs): The KPI specifications.
+        Returns:
+            list: List of KPI measures found within the analyzer directory.
+        """
+        res = []
+        for a in self.analyzer_page:
+            res.extend(a.find_kpis(kpi_specs))
+
+        if global_ignore_all_years:
+            res = KPIMeasure.remove_all_years(res)
+
+        res = KPIMeasure.remove_duplicates(res)
+        res = KPIMeasure.remove_bad_scores(res, kpi_specs.minimum_score)
+        return res
+
+    def find_multiple_kpis(self, kpi_specs_list):
+        """
+        Finds multiple KPIs within the entire analyzer directory.
+
+        Args:
+            kpi_specs_list (list): List of KPI specifications.
+
+        Returns:
+            list: List of KPI measures found within the analyzer directory.
+        """
+        res = []
+
+        for k in kpi_specs_list:
+            res.extend(self.find_kpis(k))
+
+        res = KPIMeasure.remove_bad_years(res, self.default_year)
+        res = KPIMeasure.remove_duplicates(res)
+        res = self.fix_src_name(res)
+        return res
