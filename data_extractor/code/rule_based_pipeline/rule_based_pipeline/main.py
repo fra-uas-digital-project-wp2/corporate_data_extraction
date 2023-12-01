b338e096460132fc6e398247e66fd0997f635175
ziko199 <zakaria.jouahri@outlook.de>
2023-12-01 13:44:05 +0000

    refactor

--- data_extractor/code/rule_based_pipeline/rule_based_pipeline/main.py
+++ data_extractor/code/rule_based_pipeline/rule_based_pipeline/main.py
@@ -11,7 +11,7 @@ from KPIResultSet import *
 from TestData import *
 from DataImportExport import *
 import config
-from data_extractor.code.rule_based_pipeline.rule_based_pipeline.test import test_prepare_kpispecs
+from data_extractor.code.rule_based_pipeline.rule_based_pipeline.test import test_prepare_kpispecs, load_test_data
 
 
 def generate_dummy_test_data():
@@ -30,38 +30,38 @@ def analyze_pdf(pdf_file, kpis, default_year, info_file_contents, wildcard_restr
     if guess_year is None:
         guess_year = default_year
 
-    htmldir_path = get_html_out_dir(pdf_file)
-    os.makedirs(htmldir_path, exist_ok=True)
+    html_dir_path = get_html_out_dir(pdf_file)
+    os.makedirs(html_dir_path, exist_ok=True)
 
-    reload_neccessary = True
+    reload_necessary = True
 
     if not assume_conversion_done:
         # convert pdf to html
         print_big("Convert PDF to HTML", do_wait)
-        if force_pdf_convert or not file_exists(htmldir_path + '/index.html'):
+        if force_pdf_convert or not file_exists(html_dir_path + '/index.html'):
             HTMLDirectory.convert_pdf_to_html(pdf_file, info_file_contents)
 
-        # return KPIResultSet()# STOP after converting PDF files (don't continue with analysis)
+        # return KPIResultSet() # STOP after converting PDF files (don't continue with analysis)
 
         # parse and create json and png
         print_big("Convert HTML to JSON and PNG", do_wait)
-        # print(htmldir_path)
+        # print(html_dir_path)
         dir = HTMLDirectory()
-        if (force_parse_pdf or get_num_of_files(htmldir_path + '/jpage*.json') != get_num_of_files(
-                htmldir_path + '/page*.html')):
+        if (force_parse_pdf or get_num_of_files(html_dir_path + '/jpage*.json') != get_num_of_files(
+                html_dir_path + '/page*.html')):
             dir.parse_html_directory(get_html_out_dir(pdf_file), 'page*.html')  # ! page*
-            dir.render_to_png(htmldir_path, htmldir_path)
-            dir.save_to_dir(htmldir_path)
+            dir.render_to_png(html_dir_path, html_dir_path)
+            dir.save_to_dir(html_dir_path)
             if wildcard_restrict_page == '*':
-                reload_neccessary = False
+                reload_necessary = False
 
     # return KPIResultSet()# STOP after parsing HTML files (dont continue with analysis)
 
     # load json files
     print_big("Load from JSON", do_wait)
-    if reload_neccessary:
+    if reload_necessary:
         dir = HTMLDirectory()
-        dir.load_from_dir(htmldir_path, 'jpage' + str(wildcard_restrict_page) + '.json')
+        dir.load_from_dir(html_dir_path, 'jpage' + str(wildcard_restrict_page) + '.json')
 
     # analyze
     print_big("Analyze Pages", do_wait)
@@ -69,12 +69,12 @@ def analyze_pdf(pdf_file, kpis, default_year, info_file_contents, wildcard_restr
     # kpis = test_prepare_kpispecs()
     # print(kpis)
 
-    kpiresults = KPIResultSet(ana.find_multiple_kpis(kpis))
+    kpi_results = KPIResultSet(ana.find_multiple_kpis(kpis))
 
     print_big("FINAL RESULT FOR: " + str(pdf_file.upper()), do_wait=False)
-    print_verbose(1, kpiresults)
+    print_verbose(1, kpi_results)
 
-    return kpiresults
+    return kpi_results
 
 
 def get_input_variable(val, desc):
@@ -89,7 +89,7 @@ def get_input_variable(val, desc):
 
 
 def main():
-    DEFAULT_YEAR = 2019
+    DEFAULT_YEAR = 2024
 
     parser = argparse.ArgumentParser(description='Rule-based KPI extraction')
     # Add the arguments
@@ -155,11 +155,11 @@ def main():
     pdfs = test_data.get_fixed_pdf_list()
 
     print_verbose(1, 'Related (fixed) PDFs: ' + str(pdfs) + ', in total : ' + str(len(pdfs)))
-    # return ### TODO: Uncomment this line, to return immediately, after PDF list has been shown. ###
+    # return # TODO: Uncomment this line, to return immediately, after PDF list has been shown. ###
 
     kpis = test_prepare_kpispecs()  # TODO: In the future, KPI specs should be loaded from "nicer" implemented source, e.g., JSON file definition
 
-    overall_kpiresults = KPIResultSet()
+    overall_kpi_results = KPIResultSet()
 
     info_file_contents = DataImportExport.load_info_file_contents(
         remove_trailing_slash(config.global_working_folder) + '/info.json')
@@ -167,22 +167,22 @@ def main():
     time_start = time.time()
 
     for pdf in pdfs:
-        kpiresults = KPIResultSet(kpimeasures=[])
-        cur_kpiresults = analyze_pdf(config.global_raw_pdf_folder + pdf, kpis, DEFAULT_YEAR, info_file_contents,
-                                     wildcard_restrict_page='*', assume_conversion_done=False,
-                                     force_parse_pdf=False)  ### TODO:  Modify * in order to analyze specfic page, e.g.:  *00042 ###
-        kpiresults.extend(cur_kpiresults)
-        overall_kpiresults.extend(cur_kpiresults)
-        kpiresults.save_to_csv_file(config.global_output_folder + pdf + r'.csv')
+        kpi_results = KPIResultSet(kpimeasures=[])
+        cur_kpi_results = analyze_pdf(config.global_raw_pdf_folder + pdf, kpis, DEFAULT_YEAR, info_file_contents,
+                                      wildcard_restrict_page='*', assume_conversion_done=False,
+                                      force_parse_pdf=False)  # TODO:  Modify * in order to analyze specific page, e.g.:  *00042 ###
+        kpi_results.extend(cur_kpi_results)
+        overall_kpi_results.extend(cur_kpi_results)
+        kpi_results.save_to_csv_file(config.global_output_folder + pdf + r'.csv')
         print_verbose(1, "RESULT FOR " + pdf)
-        print_verbose(1, kpiresults)
+        print_verbose(1, kpi_results)
 
     time_finish = time.time()
 
     print_big("FINAL OVERALL-RESULT", do_wait=False)
-    print_verbose(1, overall_kpiresults)
+    print_verbose(1, overall_kpi_results)
 
-    overall_kpiresults.save_to_csv_file(config.global_output_folder + r'kpiresults_tmp.csv')
+    overall_kpi_results.save_to_csv_file(config.global_output_folder + r'kpiresults_tmp.csv')
 
     total_time = time_finish - time_start
     print_verbose(1, "Total run-time: " + str(total_time) + " sec ( " + str(
